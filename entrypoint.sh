df36e6776e2b896b63d86616247ca560303958fa
GreyBalloonYU <cvdfed_19981009@126.com>
2020-07-21 19:58:38 +0800

    annotation_namespace: injector.ebupt.com

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ TLS_CERT_FILE="${TLS_CERT_FILE:-/var/lib/secrets/cert.crt}"
 TLS_KEY_FILE="${TLS_KEY_FILE:-/var/lib/secrets/cert.key}"
 CONFIGMAP_LABELS="${CONFIGMAP_LABELS:-app=k8s-sidecar-injector}"
 CONFIGMAP_NAMESPACE="${CONFIGMAP_NAMESPACE:-}"
-ANNOTATION_NAMESPACE="${ANNOTATION_NAMESPACE:-injector.tumblr.com}"
+ANNOTATION_NAMESPACE="${ANNOTATION_NAMESPACE:-injector.ebupt.com}"
 LOG_LEVEL="${LOG_LEVEL:-2}"
 echo "k8s-sidecar-injector starting at $(date) with TLS_PORT=${TLS_PORT} CONFIG_DIR=${CONFIG_DIR} TLS_CERT_FILE=${TLS_CERT_FILE} TLS_KEY_FILE=${TLS_KEY_FILE}"
 set -x
